1cbc3aa366efe46f551a5bb57a3a4b1a5c564431
nasanov <asanoff.n@gmail.com>
2025-02-25 21:23:36 -0500

    fix the public base url issue on localhost

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<BrowserRouter basename="/evil_martians_authentication_form">
-			<App />
-		</BrowserRouter>
+		<App />
 	</StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	base: '/evil_martians_authentication_form/',
+export default defineConfig(({ mode }) => {
+	return {
+		plugins: [react()],
+		base: mode === 'production' ? '/evil_martians_authentication_form/' : '/',
+	};
 });
